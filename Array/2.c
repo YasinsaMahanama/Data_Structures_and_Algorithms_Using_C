b9cc7ca7d0525ed92099af17082bc4a9cb61277d
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-11-07 13:01:35 +0530

    Create 2.c

---
+++ Array/2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int arr[5] = {2,4,3};
+    printf("The array...\n");
+    for(int i=0;i<5;i++){
+        printf("%d\n",arr[i]);
+    }
+getch();
+}

2a4687e0bfe3869ad19f3b9b06d9b997eff95c62
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2024-01-10 22:37:28 +0530

    Create enum1.c

---
+++ Enumerator/enum1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+enum State {working, failed, freezed};
+
+void main()
+{
+
+    printf("%d %d %d", working, freezed, failed);        //    0 2 1
+
+getch();
+}

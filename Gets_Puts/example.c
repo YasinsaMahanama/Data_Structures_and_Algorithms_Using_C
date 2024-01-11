b21c55caeca07e324e07db0e3164b9076efa2bb1
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2024-01-11 06:03:34 +0530

    Create example.c

---
+++ Gets_Puts/example.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char name[30];
+    printf("Enter your name : ");
+    gets(name);
+    printf("\nYour Name : ");
+    puts(name);
+getch();
+}
